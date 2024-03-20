0e3d4024b01f03dbb6d54e4ae8ab6f764e7297e5
botirovdev <botirovdev7@gmail.com>
2024-03-20 20:54:25 +0500

    m

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>404 | Usmon | MERN | ukaaa 404 diman chiqib get bi pagedan yo boshqacha sollashish garakmi!</div>
+    <div className='title'>404 | Usmon | MERN | ukaaa 404 diman chiqib get bi pagedan yo boshqacha sollashish garakmi!</div>
   )
 }
 
